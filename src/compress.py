1954f5e6dac2b8fa3a4462dead5237120b9dfb10
s70c3 <mari.s70c3@gmail.com>
2022-04-26 12:40:50 +0300

    Code for compress data.

---
+++ src/compress.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+from generate_data import render
+from tqdm import  tqdm
+from random import choice, randint
+from visualize_data import  *
+models_dir = "/Users/s70c3/Projects/SyntheticStereoDataset/data/scaled_models"
+models_files = os.listdir(models_dir)
+
+bg_dir = "/Users/s70c3/Projects/SyntheticStereoDataset/data/bgs"
+bgs = os.listdir(bg_dir)
+os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
+
+result = "./Metadata"
+from npy2pfm import writePFM, readPFM
+width, height = 960, 540
+for i in tqdm(range(150, 1001)):
+    try:
+        imgL = cv2.imread(os.path.join(result, "img/left", f"{i}.png"))
+        imgL = cv2.resize(imgL, (width, height), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(os.path.join(result, "img_scaled/left", f"{i}.png"), imgL)
+
+        imgR = cv2.imread(os.path.join(result, "img/right", f"{i}.png"))
+        imgR = cv2.resize(imgR, (width, height), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(os.path.join(result, "img_scaled/right", f"{i}.png"), imgR)
+
+        imgL = 1/(cv2.imread(os.path.join(result, "Depth", f"{i}_L.exr"), cv2.IMREAD_ANYDEPTH)/0.065/50*0.000375)
+        imgL = cv2.resize(imgL, (width, height), interpolation=cv2.INTER_AREA)
+        # imgL = np.clip(imgL, 0.0, 30.0)
+        writePFM(os.path.join(result,'pfm_scaled', f"{i}.pfm"), imgL)
+
+    # closing all open windows
+        cv2.destroyAllWindows()
+    except (ImportError, cv2.error):
+        print(i, "not found")
